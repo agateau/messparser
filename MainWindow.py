251015cfdd01e0517ed4d884d60686e09a6d677a
Aurélien Gâteau <agateau@kde.org>
2011-03-29 16:10:07 +0200

    tab => spaces

--- MainWindow.py
+++ MainWindow.py
@@ -7,52 +7,52 @@ from PyQt4.QtGui import *
 from Ui_MainWindow import Ui_MainWindow
 
 class MainWindow(QWidget, Ui_MainWindow):
-	def __init__(self):
-		QWidget.__init__(self)
-		self.setupUi(self)
+    def __init__(self):
+        QWidget.__init__(self)
+        self.setupUi(self)
 
-		QObject.connect(self.filterButton, SIGNAL("clicked()"),
-			self.applyFilter)
-		QObject.connect(self.filterComboBox.lineEdit(), SIGNAL("returnPressed()"),
-			self.applyFilter)
+        QObject.connect(self.filterButton, SIGNAL("clicked()"),
+            self.applyFilter)
+        QObject.connect(self.filterComboBox.lineEdit(), SIGNAL("returnPressed()"),
+            self.applyFilter)
 
-		self.loadSettings()
-
-
-	def closeEvent(self, event):
-		self.saveSettings()
-		event.accept()
+        self.loadSettings()
 
 
-	def applyFilter(self):
-		text = self.filterComboBox.currentText()
-		regex = re.compile(unicode(text))
-		result = [x for x in self.log if regex.search(x)]
-		self.fillView(result)
-		self.filterComboBox.addItem(text)
+    def closeEvent(self, event):
+        self.saveSettings()
+        event.accept()
 
 
-	def setLog(self, log):
-		self.log = log
-		self.fillView(self.log)
+    def applyFilter(self):
+        text = self.filterComboBox.currentText()
+        regex = re.compile(unicode(text))
+        result = [x for x in self.log if regex.search(x)]
+        self.fillView(result)
+        self.filterComboBox.addItem(text)
 
 
-	def fillView(self, lines):
-		self.logEdit.clear()
-		for line in lines:
-			self.logEdit.insertPlainText(line + "\n")
+    def setLog(self, log):
+        self.log = log
+        self.fillView(self.log)
 
 
-	def loadSettings(self):
-		settings = QSettings("bk12", "messparser")
-		lst = settings.value("ui/filters").toStringList()
-		self.filterComboBox.addItems(lst)
-		self.filterComboBox.clearEditText()
+    def fillView(self, lines):
+        self.logEdit.clear()
+        for line in lines:
+            self.logEdit.insertPlainText(line + "\n")
 
 
-	def saveSettings(self):
-		settings = QSettings("bk12", "messparser")
-		lst = QStringList()
-		for x in range(self.filterComboBox.count()):
-			lst.append(self.filterComboBox.itemText(x))
-		settings.setValue("ui/filters", QVariant(lst))
+    def loadSettings(self):
+        settings = QSettings("bk12", "messparser")
+        lst = settings.value("ui/filters").toStringList()
+        self.filterComboBox.addItems(lst)
+        self.filterComboBox.clearEditText()
+
+
+    def saveSettings(self):
+        settings = QSettings("bk12", "messparser")
+        lst = QStringList()
+        for x in range(self.filterComboBox.count()):
+            lst.append(self.filterComboBox.itemText(x))
+        settings.setValue("ui/filters", QVariant(lst))
--- messparser.py
+++ messparser.py
@@ -8,16 +8,16 @@ from MainWindow import *
 
 
 def main():
-	app = QApplication(sys.argv)
-	window = MainWindow()
+    app = QApplication(sys.argv)
+    window = MainWindow()
 
-	log = file(sys.argv[1]).readlines()
-	log = [x.rstrip() for x in log]
-	window.setLog(log)
+    log = file(sys.argv[1]).readlines()
+    log = [x.rstrip() for x in log]
+    window.setLog(log)
 
-	window.show()
-	app.exec_()
+    window.show()
+    app.exec_()
 
 
 if __name__=="__main__":
-	main()
+    main()
